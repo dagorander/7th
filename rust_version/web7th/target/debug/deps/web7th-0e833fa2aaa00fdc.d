831ff952d48ed6f84695439c45e27769076c890e
Daniel Agorander <daniel.agorander@gmail.com>
2021-10-23 15:58:30 +0200

    removing silly files

--- rust_version/web7th/target/debug/deps/web7th-0e833fa2aaa00fdc.d
+++
@@ -1,5 +0,0 @@
-/home/daniel/Projects/7th/rust_version/web7th/target/debug/deps/web7th-0e833fa2aaa00fdc.rmeta: src/main.rs
-
-/home/daniel/Projects/7th/rust_version/web7th/target/debug/deps/web7th-0e833fa2aaa00fdc.d: src/main.rs
-
-src/main.rs:
--- rust_version/web7th/target/debug/deps/web7th-96e6b4fd3013c147.d
+++
@@ -1,5 +0,0 @@
-/home/daniel/Projects/7th/rust_version/web7th/target/debug/deps/web7th-96e6b4fd3013c147.rmeta: src/main.rs
-
-/home/daniel/Projects/7th/rust_version/web7th/target/debug/deps/web7th-96e6b4fd3013c147.d: src/main.rs
-
-src/main.rs:
